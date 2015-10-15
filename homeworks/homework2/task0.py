3166d60aadcf3bd5ccc3025a8f982a764422cacf
Shenfeldgit config --global user.email shenaleksandr@mail.tu  git config --global user.name Shenfeld <shenaleksandr@mail.tu>
2015-10-15 21:55:10 +0300

    changed

--- homeworks/homework2/task0.py
+++ homeworks/homework2/task0.py
@@ -1,27 +1,24 @@
 def plural(n, words):
-    n %= 100
     n1 = n % 10
-    if 10 < n < 20:
-        return words[2]
-    elif 1 < n1 < 5:
-        return words[1]
+    n2 = n % 100
+    if n1 == 0 or 5 <= n1 <= 9 or 10 < n2 < 15:
+        return str(str(n) + " " + words[2])
     elif n1 == 1:
-        return words[0]
-    else:
-        return words[2]
+        return str(str(n) + " " + words[0])
+    elif 2 <= n1 <= 4 and (n2 != 12 or n2 != 13 or n2 != 14):
+        return str(str(n) + " " + words[1])
 words_iron = ['утюг', 'утюга', 'утюгов']
 words_spoon = ['ложка', 'ложки', 'ложек']
 words_music = ['гармошка', 'гармошки', 'гармошек']
 words_teapot = ['чайник', 'чайника', 'чайников']
 word = input()
 count = int(input())
-
 if word == 'утюг':
-    proper_word = plural(count, words_iron) 
+    proper_word = plural(count, words_iron)
 if word == 'ложка':
     proper_word = plural(count, words_spoon)
 if word == 'гармошка':
     proper_word = plural(count, words_music)
 if word == 'чайник':
     proper_word = plural(count, words_teapot)
-print('%d%s' % (count, proper_word)) 
+print (proper_word)
