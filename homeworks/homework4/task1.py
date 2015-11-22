5ac51eebeca0ebb7e18716d2b789c232b7d2682f
Shenfeldgit config --global user.email shenaleksandr@mail.tu  git config --global user.name Shenfeld <shenaleksandr@mail.tu>
2015-11-22 19:10:07 +0300

    tassssk1

--- homeworks/homework4/task1.py
+++ homeworks/homework4/task1.py
@@ -1,9 +1,10 @@
 a = open("yazkora.txt", "r")
 text = a.read().replace("\n", " ").split(".")
 out = open('answer.txt', 'w')
+space = ' '
 for sent in text:
-    for word in sent.split(' '):
+    for word in sent.split(space):
         if word.endswith ('yo'):
-            out.write(word)
+            out.write(word + space)
     out.write('\n')
 out.close()
